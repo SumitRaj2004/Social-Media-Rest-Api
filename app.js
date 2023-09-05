4941fd4e115f25270771806716bb0af897cebf12
Sumit Raj <trapgamingpubg@gmail.com>
2023-09-05 23:03:55 +0530

    made cors origin to *

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import postRouter from "./routes/postRoutes.js";
 
 const app = express();
 app.use(cors({
-    origin : "http://127.0.0.1:5500",
+    origin : "*",
     credentials : true
 }))
 app.use(express.json());
