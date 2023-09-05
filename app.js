ddf3ccf98ee91bfcec006b6c88ad7f2bfcfef8cd
Sumit Raj <trapgamingpubg@gmail.com>
2023-09-05 23:07:43 +0530

    made cors origin only to specific url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import postRouter from "./routes/postRoutes.js";
 
 const app = express();
 app.use(cors({
-    origin : "*",
+    origin : "http://127.0.0.1:1000/",
     credentials : true
 }))
 app.use(express.json());
